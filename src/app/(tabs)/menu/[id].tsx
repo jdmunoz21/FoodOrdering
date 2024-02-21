2c5104b3d6488888b52deb6ddc9e29de779577a7
jah <jdmunoz21@students.desu.edu>
2024-02-21 16:27:31 -0500

    details

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,16 +1,45 @@
 import { Stack, useLocalSearchParams } from 'expo-router';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import products from 'assets/data/products'
+import { defaultPizzaImage } from '@/src/components/ProductListItem';
+
+const sizes = ['S', 'M', 'L', 'XL'];
 
 const ProductDetailsScreen = () => {
     const {id} = useLocalSearchParams();
 
-    return (
-        <View>
-            <Stack.Screen options={{title: "Details"}} />
+    const product = products.find((p) => p.id.toString() == id);
 
-            <Text>ProductDetailsScreen for id: {id}</Text>
+    if(!product){
+        return <Text>Product not found</Text>
+    }
+
+    return (
+        <View style={styles.container}>
+            <Stack.Screen options={{title: product.name}} />
+            <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image}/>
+
+            <Text>Select size</Text>
+
+            <Text style={styles.price}>${product.price}</Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        flex: 1,
+        padding: 10
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1
+    },
+    price: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+});
 
 export default ProductDetailsScreen;
